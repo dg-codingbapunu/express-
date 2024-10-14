5f66414964e8f5804a39db8d33db7a6a93a9d220
patitapaban <patitapabanpanda046@gmail.com>
2024-10-14 10:46:07 +0530

    dyanamic route

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const http = require('http');
 const fs = require('fs')
 
  const index = fs.readFileSync('index.html', 'utf-8');
-const data = fs.readFileSync('data.json', 'utf-8')
-
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const product = data.products[0];
 
 const server = http.createServer((req, res) => {
 console.log(req.url);
@@ -18,9 +18,16 @@ case '/api':
 
 res.setHeader('Content-type', 'application/json')
 
-res.end(data);
+res.end(JSON.stringify(data));
   break;
 
+  case '/product':
+
+  res.setHeader('Content-type', 'text/html')
+
+   let mindex = index.replace('**title**', product.title)
+    res.end(mindex);
+break;
 default:
     res.writeHead(404,'NT_FOND');
     res.end();
