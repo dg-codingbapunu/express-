6dc461f8566b1296f8c4c17f6f921bb35a9bd01b
patitapaban <patitapabanpanda046@gmail.com>
2024-10-14 09:42:44 +0530

    send response to server

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const { json } = require('express/lib/response');
-const http = require('http');
 
-const data = {age:5}
+const http = require('http');
+const fs = require('fs')
+
+ const index = fs.readFileSync('index.html', 'utf-8');
+const data= fs.readFileSync('data.json', 'utf-8');
+
+// const data = {age:5}
 
 const server = http.createServer((req, res) => {
+console.log(req.url);
 
 console.log('server started');
 
-res.setHeader('Dummy', 'DummyValue')
-res.setHeader('Content-Type', 'application/json')
-res.end(JSON.stringify(data));
+// res.setHeader('Dummy', 'DummyValue')
+res.setHeader('Content-type', 'application/json')
+// res.setHeader('Content-Type', 'text/html')
+res.end(data);
 
 
 })
