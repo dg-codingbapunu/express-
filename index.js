c8edde0cab89f3f98f7129bb7ec0eaa31034931b
patitapaban <patitapabanpanda046@gmail.com>
2024-10-14 10:21:11 +0530

    server side rendering

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const http = require('http');
 const fs = require('fs')
 
  const index = fs.readFileSync('index.html', 'utf-8');
-const data= fs.readFileSync('data.json', 'utf-8');
+const data = fs.readFileSync('data.json', 'utf-8')
 
-// const data = {age:5}
 
 const server = http.createServer((req, res) => {
 console.log(req.url);
 
+switch(req.url){
+  case '/':
+    res.setHeader('Content-Type', 'text/html');
+    res.end(index);
+   break;
+case '/api':
+
+res.setHeader('Content-type', 'application/json')
+
+res.end(data);
+  break;
+
+default:
+    res.writeHead(404,'NT_FOND');
+    res.end();
+}
+
 console.log('server started');
 
 // res.setHeader('Dummy', 'DummyValue')
-res.setHeader('Content-type', 'application/json')
-// res.setHeader('Content-Type', 'text/html')
-res.end(data);
+
+
+
 
 
 })
