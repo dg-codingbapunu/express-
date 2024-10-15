edd0d4a800d07b31975877e52ceef0394e211c69
patitapaban <patitapabanpanda046@gmail.com>
2024-10-15 10:31:52 +0530

    res method in express

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
-// const index = fs.readFileSync('index.html', 'utf-8');
-// const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-// const products = data.products;
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
+
+    // new  code -------////   ------------
+
   const express = require('express');
 
   const server = express();
 
-  
+    server.get('/',(req,res)=>{
+
+      // res.sendStatus(200);
+
+      res.json(products);
+
+      // res.send('<h1>hello</h1>');
+
+      // res.sendFile('C:\Users\pp195\OneDrive\Desktop\express js\index.html');
+
+    })
 
 
 
