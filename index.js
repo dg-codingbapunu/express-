9e256b519baae4c5ae4a89d34f43551d6e2dbb67
patitapaban <patitapabanpanda046@gmail.com>
2024-10-15 09:41:24 +0530

    serverside rendering

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 
- const index = fs.readFileSync('index.html', 'utf-8');
+const index = fs.readFileSync('index.html', 'utf-8');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const product = data.products[0];
+const products = data.products;
 
 const server = http.createServer((req, res) => {
-console.log(req.url);
+  console.log(req.url, req.method);
 
-switch(req.url){
-  case '/':
+  if(req.url.startsWith('/product')){
+    const id = req.url.split('/')[2]
+    const product = products.find(p=>p.id===(+id))
+    console.log(product)
     res.setHeader('Content-Type', 'text/html');
-    res.end(index);
-   break;
-case '/api':
-
-res.setHeader('Content-type', 'application/json')
-
-res.end(JSON.stringify(data));
-  break;
-
-  case '/product':
-
-  res.setHeader('Content-type', 'text/html')
-
-   let mindex = index.replace('**title**', product.title)
-    res.end(mindex);
-break;
-default:
-    res.writeHead(404,'NT_FOND');
-    res.end();
-}
-
-console.log('server started');
-
-// res.setHeader('Dummy', 'DummyValue')
+          let modifiedIndex = index.replace('**title**', product.title)
+          .replace('**url**', product.thumbnail)
+          .replace('**price**', product.price)
+          .replace('**rating**', product.rating)
+          res.end(modifiedIndex);
+          return;
+  }
 
 
 
 
+  switch (req.url) {
+    case '/':
+      res.setHeader('Content-Type', 'text/html');
+      res.end(index);
+      break;
+    case '/api':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(data));
+      break;
 
-})
+    default:
+      res.writeHead(404);
+      res.end();
+  }
+
+  console.log('server started  ');
+  //   res.setHeader('Dummy', 'DummyValue');
+
+  //
+});
 
 server.listen(8080);
