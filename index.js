ade0895a47ed18de144829320e2bd68ccde6cee6
patitapaban <patitapabanpanda046@gmail.com>
2024-10-15 10:38:25 +0530

    api test in post man

--- index.js
+++ index.js
@@ -6,14 +6,67 @@ const products = data.products;
     // new  code -------////   ------------
 
   const express = require('express');
+const { type } = require('os');
 
   const server = express();
 
-    server.get('/',(req,res)=>{
+  server.get('/',(req,res)=> {
+    res.json({type:'GET'})
+  })
+
+
+  server.post('/',(req,res)=> {
+    res.json({type:'POST'})
+  })
+
+
+  server.put('/',(req,res)=> {
+    res.json({type:'PUT'})
+  })
+
+
+
+
+  server.delete('/',(req,res)=> {
+    res.json({type:'DELETE'})
+  })
+
+
+
+
+
+  server.patch('/',(req,res)=> {
+    res.json({type:'PATCH'})
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    server.get('/demo',(req,res)=>{
 
       // res.sendStatus(200);
 
-      res.json(products);
+      // res.json(products);
 
       // res.send('<h1>hello</h1>');
 
