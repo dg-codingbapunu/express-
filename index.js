f66f91f4bbac1a184b6fe0d355cd9be8f9453d74
patitapaban <patitapabanpanda046@gmail.com>
2024-10-17 10:47:26 +0530

    destructure code

--- index.js
+++ index.js
@@ -3,29 +3,33 @@ const controller = require('./controller/product.js')
   const express = require('express');
     const morgan = require('morgan');
 
+ 
+
   const server = express();
   // server.use(morgan('default'));
   server.use(express.json()); // body parser
 
     server.use(express.static('public'));// static hoisting middleware
-
+    const productRouter = express.Router();
+   
+    server.use('/',productRouter);
 
 
 
 
    // create/post/products
 
+   productRouter
+  .post('/products',controller.createPost)// create
 
-  server.post('/products',controller.createPost); // create
+  .get('/products',controller.getAllProducts) // read all products
 
-  server.get('/products',controller.getAllProducts); // read all products
+  .get('/products/:id',controller.getSingleProduct) // read single product
 
-  server.get('/products/:id',controller.getSingleProduct) // read single product
-
-  server.put('/products/:id',controller.replaceProduct); // update product
+  .put('/products/:id',controller.replaceProduct) // update product
 
 
-  server.delete('/products/:id',controller.deleteProduct) // delete product
+.delete('/products/:id',controller.deleteProduct) // delete product
 
 
 
