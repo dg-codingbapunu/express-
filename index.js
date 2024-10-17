63989cd2154f69d9c3b14aa0b9e8cab5749a68d5
patitapaban <patitapabanpanda046@gmail.com>
2024-10-17 10:13:09 +0530

    mvc file structure

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const fs = require('fs');
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const products = data.products;
+
 
     // new  code -------////   ------------
 
@@ -13,60 +10,6 @@ const products = data.products;
   server.use(express.json()); // body parser
 
     server.use(express.static('public'));// static hoisting middleware
-
-
-    const createPost = (req,res)=> {
-
-      console.log(req.body);
-      products.push(req.body);
-  
-      res.status(201).json(req.body);
-    }
-
-
-
-
-    const getAllProducts = (req,res)=> {
-   
-      res.json(products) // read api Get /products
-      
-    }
-
-
-    const getSingleProduct = (req,res)=> {
-   
-      const id = +req.params.id;
-    const product =   products.find(p=>p.id == id)
-  
-       res.json(product) // to read or get one product
-      
-    }
-
-
-    const replaceProduct = (req,res)=> {
-   
-      const id = +req.params.id;
-    const productIndex =   products.findIndex(p=>p.id === id)
-    products.splice(productIndex,1,{...req.body,id:id})
-    
-    
-       res.status(201).json() ;
-      
-    }
-
-
-
-    const deleteProduct = (req,res)=> {
-
-      const id = + req.params.id;
-  
-      const productIndex = products.findIndex(p=> p.id === id)
-       const product = products[productIndex]
-      products.splice(productIndex,1)
-  
-      res.status(201).json(product)
-  
-    }
 
 
 
@@ -97,34 +40,6 @@ const products = data.products;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    server.get('/demo',(req,res)=>{
-
-      // res.sendStatus(200);
-
-      // res.json(products);
-
-      // res.send('<h1>hello</h1>');
-
-      // res.sendFile('C:\Users\pp195\OneDrive\Desktop\express js\index.html');
-
-    })
 
 
 
