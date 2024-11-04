6cb87ca93bf896e3a2f3bbbae3890d7befb5f782
patitapaban <patitapabanpanda046@gmail.com>
2024-11-04 12:01:27 +0530

    serverside rendering

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
-  console.log('database connected')
+  console.log('database connected securely')
 }
 //Schema
 
